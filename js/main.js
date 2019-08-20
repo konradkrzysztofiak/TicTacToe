0bdf83e11eb96c52e03bb52d374113547742afbf
bq666 <wojciech.gaudnik@gmail.com>
2019-08-20 09:17:35 +0200

    Add eventLisistiner to squer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+let square = document.querySelectorAll(".square");
 window.onload = function () {
-    let square = document.querySelectorAll(".square");
-    console.log(square[0]);
+    // console.log(square[0]);
     for (let i = 0; i < square.length; i++) {
         square[i].onclick = function(){
-            alert("asdgasg");
+            console.log(square[i]);
+            square[i].querySelector("p").innerHTML = "X";
+            console.log(square[i].tagName)
+            // alert("asdgasg");
         }
     }
-}
+};
+
+
 
