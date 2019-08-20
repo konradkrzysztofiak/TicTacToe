2c1eeee21283a92aa624536793f5ace9ca8ac197
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-08-20 22:00:27 +0200

    Add reset button and marks

--- js/main.js
+++ js/main.js
@@ -1,46 +1,34 @@
-let board = [
-    ["1", "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"],
-];
-let turn = false;   //todo true human, false AI
-
 window.onload = function (){
-    let square = document.querySelectorAll(".square");
-    console.log(square.keys());
-    // refreshBoard(board);
-    for (let i = 0; i < square.length; i++) {
-        square[i].onclick = function(){
-            square[i].querySelector("p").innerHTML = "X";
-            console.log(square[i].getAttribute("value"));
+    let board = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "],
+    ];
+
+    let turn = false;   //todo true human, false AI
+    let allHtmlSquares = document.querySelectorAll(".square");
+    document.querySelector("#btnReset").addEventListener("click", function (event) {
+        refreshBoard(board, allHtmlSquares)
+
+    });
+
+    for (let i = 0; i < allHtmlSquares.length; i++) {
+        allHtmlSquares[i].onclick = function(){
+            allHtmlSquares[i].querySelector("p").innerHTML = "X";
+            console.log(allHtmlSquares[i].getAttribute("value"));
         }
     }
 };
 
-function main() {
-    console.log("Runnnnnn <--------");
-    console.log(document.querySelector("#board"));
-}
 
-// document.onload();
-
-function refreshBoard(inBoard) {
+function refreshBoard(inBoard, htmlSquares) {
+    console.log("refreshBoard");
     let z = 0;
-    console.log(square.length);
-    console.log(square[0] + " <---->");
-    for (let y = 0; y < square.length; y++) {
+    for (let y = 0; y < 3; y++) {
         for (let x = 0; x < 3; x++) {
             console.log(inBoard[y][x]);
-            square[z++].querySelector("p").innerHTML = inBoard[y][x++];
+            htmlSquares[z++].querySelector("p").innerHTML = inBoard[y][x];
         }
-        // square[y].querySelector("p").innerHTML = inBoard[y][x++];
-        // square[y].querySelector("p").innerHTML = inBoard[y][x++];
-        // x = 0;
-        // square[i].onclick = function(){
-        //     square[i].querySelector("p").innerHTML = "X";
-        //     console.log(square[i].getAttribute("value"));
-        // }
     }
-
 }
 
