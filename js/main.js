3cfe53e74fc31bb6211e557e359510b555b90ca4
bq666 <wojciech.gaudnik@gmail.com>
2019-08-20 16:47:13 +0200

    broken ruan

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 let board = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "],
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
 ];
 let turn = false;   //todo true human, false AI
-let square = document.querySelectorAll(".square");
 
 
-window.onload = function () {
+document.onload = function () {
+
+    console.log("ttteeesssttt");
+    console.log("Runnnnnn <--------");
+    let square = document.querySelectorAll(".square");
+    console.log(square[0]);
+    // refreshBoard(board);
     for (let i = 0; i < square.length; i++) {
         square[i].onclick = function(){
             square[i].querySelector("p").innerHTML = "X";
@@ -16,7 +21,25 @@ window.onload = function () {
     }
 };
 
-function refreshBoard() {
+document.onload();
+
+function refreshBoard(inBoard) {
+    let z = 0;
+    console.log(square.length);
+    console.log(square[0] + " <---->");
+    for (let y = 0; y < square.length; y++) {
+        for (let x = 0; x < 3; x++) {
+            console.log(inBoard[y][x]);
+            square[z++].querySelector("p").innerHTML = inBoard[y][x++];
+        }
+        // square[y].querySelector("p").innerHTML = inBoard[y][x++];
+        // square[y].querySelector("p").innerHTML = inBoard[y][x++];
+        // x = 0;
+        // square[i].onclick = function(){
+        //     square[i].querySelector("p").innerHTML = "X";
+        //     console.log(square[i].getAttribute("value"));
+        // }
+    }
 
 }
 
