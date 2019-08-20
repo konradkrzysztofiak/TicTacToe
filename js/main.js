8aae74c4c2bb113740b2052dff350331e241680c
bq666 <wojciech.gaudnik@gmail.com>
2019-08-20 11:24:40 +0200

    add global variable

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
+let board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+];
+let turn = false;   //todo true human, false AI
 let square = document.querySelectorAll(".square");
+
+
 window.onload = function () {
-    // console.log(square[0]);
     for (let i = 0; i < square.length; i++) {
         square[i].onclick = function(){
-            console.log(square[i]);
             square[i].querySelector("p").innerHTML = "X";
-            console.log(square[i].tagName)
-            // alert("asdgasg");
+            console.log(square[i].getAttribute("value"));
         }
     }
 };
 
+function refreshBoard() {
 
+}
 
