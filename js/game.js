bb3d86da919b9e942386339c933919ea9f2db821
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-08-21 12:02:22 +0200

    Add winning condition

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ let difficultMap = new Map([[1, "Easy"], [2, "Medium"], [3, "Hard"]]);
 let userSign = "X";
 let turn = true;   //todo true human, false AI
 let difficulty = 1; //todo 0 random 1 smarter 2 the smartest
+let gameStart = true;
 let board = [
     [" ", " ", " "],
     [" ", " ", " "],
@@ -12,6 +13,18 @@ let freshBoard = [
     [" ", " ", " "],
     [" ", " ", " "],
 ];
+
+let coordinates = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2]
+};
 
 
 
@@ -46,10 +59,13 @@ window.onload = function (){
     //todo mark X
     for (let i = 0; i < htmlAllSquares.length; i++) {
         htmlAllSquares[i].onclick = function(){
-            if (turn && htmlAllSquares[i].querySelector("p").innerHTML !== "O") {
-                htmlAllSquares[i].querySelector("p").innerHTML = "X";
+            if (gameStart) {
+                if (turn && htmlAllSquares[i].querySelector("p").innerHTML !== "O") {
+                    htmlAllSquares[i].querySelector("p").innerHTML = "X";
+                    fillBoard(coordinates[this.id], "X");
+                    checkWin();
+                }
             }
-            console.log(htmlAllSquares[i].getAttribute("value"));
         }
     }
 };
@@ -88,3 +104,45 @@ function setUserSignO(htmlYourSign) {
     userSign = "O";
     htmlYourSign.innerHTML = "<p>Your sign: " + userSign + "</p>";
 }
+function fillBoard(coordinates, value) {
+    board[coordinates[0]][coordinates[1]] = value;
+}
+
+function checkWin() {
+
+    if (checkRow() || checkColumn()) {
+        alert("U win!");
+        gameStart = false;
+    }
+
+}
+
+function checkRow() {
+    for (let j = 0; j < board.length; j++) {
+        for (let i = 0; i <= board[j].length; i++) {
+            if (board[j][i] !== " ") {
+                let firstInRow = board[j][0];
+                if (board[j][i + 1] === firstInRow && board[j][i + 2] === firstInRow) {
+                    return true;
+                }
+            }
+        }
+    }
+}
+
+function checkColumn() {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++) {
+            if (board[j][i] !== " ") {
+                let firstInColumn = board[j][i];
+                if (j < 1) {
+                    if (board[j + 1][i] === firstInColumn && board[j + 2][i] === firstInColumn) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
