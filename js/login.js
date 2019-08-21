b47cee49a6e86af270a543eab5c5c54088d11c61
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-08-21 15:15:54 +0200

    Create design of website and prepare for new game creator

--- js/login.js
+++ js/login.js
@@ -1,12 +1,21 @@
 window.onload = function () {
     let btnLogin = document.querySelector(".login-btn");
     let nick = document.querySelector("#nick");
+    let newGame = document.querySelector("#newGame");
     btnLogin.addEventListener('click', function () {
         console.log("test  " + nick.value);
         localStorage.nick = nick.value;
         console.log(localStorage.nick);
         // passNick(nick);
     });
+    newGame.addEventListener('click', function () {
+        if (document.getElementById("createNewGame").style.display === "none"){
+            document.getElementById("createNewGame").style.display = "block";
+        } else {
+            document.getElementById("createNewGame").style.display = "none"
+        }
+
+    })
 };
 
 // function passNick(nick) {
