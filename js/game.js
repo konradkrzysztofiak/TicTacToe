c30105f6c483a6a1bfffad468966a53e0641bd2a
bq666 <wojciech.gaudnik@gmail.com>
2019-08-21 10:06:08 +0200

    added increasing and decreasing difficulty

--- js/game.js
+++ js/game.js
@@ -1,37 +1,65 @@
+let difficultMap = new Map([[1, "Easy"], [2, "Medium"], [3, "Hard"]]);
+let turn = true;   //todo true human, false AI
+let difficulty = 1; //todo 0 random 1 smarter 2 the smartest
+let board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+];
+
+
+
+
 window.onload = function (){
-    let board = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "],
-    ];
+    let htmlAllSquares = document.querySelectorAll(".square");
+    let htmlDifficulty = document.querySelector("#difficultMessage");
+    document.querySelector("#welcomeMessage").innerHTML = "<p>" + localStorage.nick + " Let's play !</p>";
+    htmlDifficulty.innerHTML = "<p>Difficulty: " + difficultMap.get(difficulty) + "</p>";
 
-    let turn = true;   //todo true human, false AI
-    let allHtmlSquares = document.querySelectorAll(".square");
-    document.querySelector("#btnReset").addEventListener("click", function (event) {
-        refreshHtmlBoard(board, allHtmlSquares)
+    document.querySelector("#btc-increase").addEventListener("click", function () {
+        increaseDifficulty(htmlDifficulty);
     });
-    let welcomeMessage = document.querySelector("#welcomeMessage").innerHTML = "<p>" + localStorage.nick + " Let's play !</p>";
+    document.querySelector("#btc-decrease").addEventListener("click", function () {
+        decreaseDifficulty(htmlDifficulty);
+    });
+    document.querySelector("#btnReset").addEventListener("click", function (event) {
+        refreshHtmlBoard(htmlAllSquares)
+    });
 
 
-    for (let i = 0; i < allHtmlSquares.length; i++) {
-        allHtmlSquares[i].onclick = function(){
-            if (turn && allHtmlSquares[i].querySelector("p").innerHTML !== "O") {
-                allHtmlSquares[i].querySelector("p").innerHTML = "X";
+    //todo mark X
+    for (let i = 0; i < htmlAllSquares.length; i++) {
+        htmlAllSquares[i].onclick = function(){
+            if (turn && htmlAllSquares[i].querySelector("p").innerHTML !== "O") {
+                htmlAllSquares[i].querySelector("p").innerHTML = "X";
             }
-            console.log(allHtmlSquares[i].getAttribute("value"));
+            console.log(htmlAllSquares[i].getAttribute("value"));
         }
     }
 };
 
 
-function refreshHtmlBoard(inBoard, htmlSquares) {
-    console.log("refreshBoard");
+function refreshHtmlBoard(htmlSquares) {
     let z = 0;
     for (let y = 0; y < 3; y++) {
         for (let x = 0; x < 3; x++) {
-            console.log(inBoard[y][x]);
-            htmlSquares[z++].querySelector("p").innerHTML = inBoard[y][x];
+            console.log(board[y][x]);
+            htmlSquares[z++].querySelector("p").innerHTML = board[y][x];
         }
     }
 }
 
+
+function increaseDifficulty(htmlIncrease) {
+    console.log(difficulty);
+    if (difficulty < 3 && difficulty++) {
+        htmlIncrease.innerHTML = "<p>Difficulty: " + difficultMap.get(difficulty) + "</p>";
+    }
+}
+
+function decreaseDifficulty(htmlIncrease) {
+    console.log(difficulty);
+    if (difficulty > 1 && difficulty--) {
+        htmlIncrease.innerHTML = "<p>Difficulty: " + difficultMap.get(difficulty) + "</p>";
+    }
+}
