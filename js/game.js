24bb6daff8b870d3152ea1bf0e0d7335af578b24
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-08-22 20:33:49 +0200

    added JSON sort objects

--- js/game.js
+++ js/game.js
@@ -27,17 +27,27 @@ let coordinates = {
 
 window.onload = function () {
     //todo how we should storage our's all players
-    var players = {
-        "adam": 1,
-        "ewa": 2,
-        "ala": 3
-    };
+    var players = [
+        {playerName: "Adam0", score: 6666},
+        {playerName: "Adam1", score: 3},
+        {playerName: "Adam666", score: 4},
+        {playerName: "Adam3", score: 2},
+        {playerName: "Adam4", score: 1}
+    ];
+
+    function compare(a, b) {
+        return b.score - a.score;
+    }
+
+    players.sort(compare);
     localStorage.players = JSON.stringify(players);
+
+
 
     //todo how we get players and theirs score from local store
     var playersFromLocalStorage = JSON.parse(localStorage.players);
-    for (var keyPlayer in playersFromLocalStorage) {
-        console.log(keyPlayer + " " + playersFromLocalStorage[keyPlayer]);
+    for (let i = 0; i < playersFromLocalStorage.length; i++) {
+        console.log(playersFromLocalStorage[i].playerName + " " + playersFromLocalStorage[i].score);
     }
 
     console.log("-------------------------------------------------------------------");
