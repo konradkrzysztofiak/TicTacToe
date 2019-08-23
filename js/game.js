4faaec1e845547fb497db4816656a0bb21f187f3
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-08-23 07:34:57 +0200

    added hard coded hard

--- js/game.js
+++ js/game.js
@@ -108,7 +108,7 @@ window.onload = function () {
                 } else if (difficulty === "medium") {
                     turnAIMedium(htmlAllSquares);
                 } else {
-                    turnAIDummy(htmlAllSquares);
+                    turnAIHard(htmlAllSquares);
                 }
                 refreshHtmlBoard(htmlAllSquares, board);
                 turn = "human";
@@ -228,6 +228,110 @@ function turnAIMedium(htmlAllSquares) {
     }
 }
 
+function turnAIHard(htmlAllSquares) {
+    let AISign = (userSign === "X") ? "O" : "X";
+    if (board[1][1] === " ") {
+        board[1][1] = AISign;
+        return;
+    }
+    while (true) {
+        for (let y = 0; y < 3; y++) {
+            if (board[y][0] === AISign && board[y][1] === AISign && board[y][2] === " ") {
+                board[y][2] = AISign;
+                return;
+            }
+            if (board[y][1] === AISign && board[y][2] === AISign && board[y][0] === " ") {
+                board[y][0] = AISign;
+                return;
+            }
+            if (board[y][2] === AISign && board[y][0] === AISign && board[y][1] === " ") {
+                board[y][1] = AISign;
+                return;
+            }
+            if (board[0][y] === AISign && board[1][y] === AISign && board[2][y] === " ") {
+                board[2][y] = AISign;
+                return;
+            }
+            if (board[1][y] === AISign && board[2][y] === AISign && board[0][y] === " ") {
+                board[0][y] = AISign;
+                return;
+            }
+            if (board[2][y] === AISign && board[0][y] === AISign && board[0][y] === " ") {
+                board[1][y] = AISign;
+                return;
+            }
+        }
+        if (board[0][0] === AISign && board[1][1] === AISign && board[2][2] === " ") {
+            board[2][2] = AISign;
+            return;
+        }
+        if (board[0][2] === AISign && board[1][1] === AISign && board[2][0] === " ") {
+            board[2][0] = AISign;
+            return;
+        }
+        if (board[2][2] === AISign && board[1][1] === AISign && board[0][0] === " ") {
+            board[0][0] = AISign;
+            return;
+        }
+        if (board[2][0] === AISign && board[1][1] === AISign && board[0][2] === " ") {
+            board[0][2] = AISign;
+            return;
+        }
+
+        for (let y = 0; y < 3; y++) {
+            if (board[y][0] === userSign && board[y][1] === userSign && board[y][2] === " ") {
+                board[y][2] = AISign;
+                return;
+            }
+            if (board[y][1] === userSign && board[y][2] === userSign && board[y][0] === " ") {
+                board[y][0] = AISign;
+                return;
+            }
+            if (board[y][2] === userSign && board[y][0] === userSign && board[y][1] === " ") {
+                board[y][1] = AISign;
+                return;
+            }
+            if (board[0][y] === userSign && board[1][y] === userSign && board[2][y] === " ") {
+                board[2][y] = AISign;
+                return;
+            }
+            if (board[1][y] === userSign && board[2][y] === userSign && board[0][y] === " ") {
+                board[0][y] = AISign;
+                return;
+            }
+            if (board[2][y] === userSign && board[0][y] === userSign && board[0][y] === " ") {
+                board[1][y] = AISign;
+                return;
+            }
+        }
+        if (board[0][0] === userSign && board[1][1] === userSign && board[2][2] === " ") {
+            board[2][2] = AISign;
+            return;
+        }
+        if (board[0][2] === userSign && board[1][1] === userSign && board[2][0] === " ") {
+            board[2][0] = AISign;
+            return;
+        }
+        if (board[2][2] === userSign && board[1][1] === userSign && board[0][0] === " ") {
+            board[0][0] = AISign;
+            return;
+        }
+        if (board[2][0] === userSign && board[1][1] === userSign && board[0][2] === " ") {
+            board[0][2] = AISign;
+            return;
+        }
+
+
+        let y = Math.round(Math.random() * 2);
+        let x = Math.round(Math.random() * 2);
+        if (board[y][x] === " ") {
+            board[y][x] = (userSign === "X") ? "O" : "X";
+            break;
+        }
+    }
+}
+
+
 function checkDraw() {
     for (let y = 0; y < 3; y++) {
         for (let x = 0; x < 3; x++) {
@@ -315,6 +419,167 @@ function checkDiagonal() {
         }
     }
 }
+
+
+//todo ------------------HARD--------------------
+function turnAiHard(htmlAllSquares) {
+
+
+    while (true) {
+
+        for (let i = 0; i < startingPos.length; i++) {
+            if (!turn) {
+                if (!firstMoveOfHardAi) {
+                    if (board[coordinates[startingPos[i]][0]][coordinates[startingPos[i]][1]] === " ") {
+                        startHardAiPos = startingPos[i];
+                        console.log("hello");
+                        board[coordinates[startHardAiPos][0]][coordinates[startHardAiPos][1]] = (userSign === "X") ? "O" : "X";
+                        firstMoveOfHardAi = true;
+                        turn = true;
+                        break;
+                    }
+                } else {
+                    blockPlayerMoveHorizontal();
+                    refreshHtmlBoard(htmlAllSquares, board);
+                    moveHorizontal(htmlAllSquares);
+
+                    turn = true;
+                    break;
+                }
+            }
+        }
+        break;
+
+
+    }
+
+    refreshHtmlBoard(htmlAllSquares, board);
+    if (checkWin()) {
+        gameInProgress = false;
+        winning = "AI hard";
+    }
+    turn = true;
+}
+
+function moveHorizontal(htmlAllSquares) {
+    if (!turn) {
+        console.log("moveHorizontal");
+        startHardAiPos = startHardAiPos + 3;
+        if (board[coordinates[startHardAiPos][0]][coordinates[startHardAiPos][1]] === " ") {
+            board[coordinates[startHardAiPos][0]][coordinates[startHardAiPos][1]] = (userSign === "X") ? "O" : "X";
+            turn = true;
+        } else {
+            console.log("moveHorizontalFalse");
+            //firstMoveOfHardAi = false;
+            //turnAiHard(htmlAllSquares);
+        }
+    }
+}
+
+function blockPlayerMoveHorizontal() {
+
+    if (!turn) {
+        console.log("blockHorizontal");
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board.length; j++) {
+                if (board[j][i] === userSign || board[j][i] === " ") {
+                    if (j < 1) {
+                        if (board[j][i] === userSign && board[j + 1][i] === userSign && board[j + 2][i] === " ") {
+                            board[j + 2][i] = "O";
+                            turn = true;
+                        }
+                    } else if (j < 2) {
+                        if (board[j][i] === userSign && board[j + 1][i] === userSign && board[j - 1][i] === " ") {
+                            board[j - 1][i] = "O";
+                            turn = true;
+                        }
+
+                    }
+                } else {
+                    blockPlayerMoveDiagonal();
+                }
+            }
+        }
+    }
+}
+
+function blockPlayerMoveDiagonal() {
+    if (!turn && checkPlayerMoveDiagonal()) {
+        console.log("blockDiagonal");
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board.length; j++) {
+                if (board[j][i] === userSign) {
+                    if (j < 1) {
+                        if (board[j][i] === userSign && board[j + 1][i + 1] === userSign) {
+                            board[j + 2][i + 2] = "O";
+                            turn = true;
+                        } else if (board[j][i] === userSign && board[j + 2][i - 1] === userSign) {
+                            board[j + 2][i - 2] = "O";
+                            turn = true;
+                        }
+                    } else if (j < 2) {
+                        if (board[j][i] === userSign && board[j + 1][i - 1] === userSign && board[j - 1][i + 1] === " ") {
+                            board[j - 1][i + 1] = "O";
+                            turn = true;
+                        } else if (board[j][i] === userSign && board[j + 1][i - 1] === userSign && board[j - 1][i + 1] === " ") {
+                            board[j - 1][i + 1] = "O";
+                            turn = true;
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        blockPlayerMoveVertical();
+    }
+}
+
+function checkPlayerMoveDiagonal() {
+    if (!turn) {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board.length; j++) {
+                if (board[j][i] === userSign) {
+                    if (j < 1) {
+                        if (board[j][i] === userSign && board[j + 1][i + 1] === userSign && board[j + 2][i + 2] === " ") {
+                            return true;
+                        } else if (board[j][i] === userSign && board[j + 2][i - 1] === userSign && board[j + 2][i - 2] === " ") {
+                            return true;
+                        }
+                    } else if (j < 2) {
+
+                        if (board[j][i] === userSign && board[j + 1][i - 1] === userSign && board[j - 1][i + 1] === " ") {
+                            return true;
+                        } else if (board[j][i] === userSign && board[j - 1][i + 1] === userSign && board[j - 1][i + 1] === " ") {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
+
+function blockPlayerMoveVertical() {
+    if (!turn) {
+        console.log("blockVertical");
+        for (let j = 0; j < board.length; j++) {
+            for (let i = 0; i <= board.length; i++) {
+                console.log("hello");
+                if (board[j][i] === userSign) {
+                    if (board[j][i] === userSign && board[j][i] === userSign && board[j][i + 2] === " ") {
+                        board[j][i + 2] = (userSign === "X") ? "O" : "X";
+                        turn = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
 
 // function increaseDifficulty(htmlIncrease) {
 //     if (difficulty < 3 && difficulty++) {
